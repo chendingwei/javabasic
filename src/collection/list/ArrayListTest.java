89669c2976884b3fc83702f890065c3aeb0fd89b
cdw <chendingwei>
2020-08-12 21:15:36 +0800

    1

---
+++ src/collection/list/ArrayListTest.java
@@ -0,0 +1,12 @@
+package collection.list;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+
+
+    }
+}
---
+++ src/collection/list/VectorTest.java
@@ -0,0 +1,9 @@
+package collection.list;
+
+import java.util.Vector;
+
+public class VectorTest {
+    public static void main(String[] args) {
+        Vector<Integer> integers = new Vector<>();
+    }
+}
