c5a811f2ab7f578992c591d575bb291ba85b75d0
cdw <chendingwei>
2020-08-24 20:19:51 +0800

    1

--- src/basicalgorithm/sort/advancedsort/Merge.java
+++ src/basicalgorithm/sort/advancedsort/Merge.java
@@ -57,7 +57,7 @@ public class Merge {
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {8,7,6,5,4,3,2,1};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
--- src/recite/Quick.java
+++ src/recite/Quick.java
@@ -1,4 +1,55 @@
 package recite;
 
+import java.util.Arrays;
+
 public class Quick {
+    public static void sort(int[] arr){
+        int left =0;
+        int right = arr.length-1;
+        sort(arr,left,right);
+    }
+
+    public static void sort(int[] arr, int left, int right){
+        if(right <= left){
+            return;
+        }
+        int partition = partition(arr, left, right);
+        sort(arr,left,partition-1);
+        sort(arr,partition+1,right);
+    }
+
+    public static int partition(int[] arr,int left, int right){
+        int key = arr[left];
+        int p1 = left;
+        int p2 = right+1;
+        while (true){
+            while (key < arr[--p2]){
+                if(p2 == left){
+                    break;
+                }
+            }
+            while (arr[++p1] < key){
+                if(p1 == right){
+                    break;
+                }
+            }
+            if(left >= right){
+                break;
+            }else {
+                int temp = arr[p1];
+                arr[p1] = arr[p2];
+                arr[p2] = temp;
+            }
+        }
+        int temp = arr[left];
+        arr[left] = arr[p2];
+        arr[p2] = temp;
+        return p2;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
